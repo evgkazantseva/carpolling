d7af496473aba98702d0043b24549b2d857eb498
Evgeniia Kazantceva <evg.kazantseva@gmail.com>
2023-06-16 12:54:30 +0500

    fix trips

--- service/journey/urls.py
+++ service/journey/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserProfileViewSet, TripViewSet, UserCreateAPIView, LoginView
+from .views import UserProfileView, TripView, UserCreateAPIView, LoginView
 
 router = DefaultRouter()
-router.register('trips', TripViewSet, basename='trip')
+# router.register('trips', TripViewSet, basename='trip')
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('trips/join_trip/', TripViewSet.as_view({'post': 'join_trip'}), name='join_trip'),
+    # path('trips/join_trip/', TripViewSet.as_view({'post': 'join_trip'}), name='join_trip'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('user/profile/', UserProfileViewSet.as_view(), name='user-profile'),
+    path('user/profile/', UserProfileView.as_view(), name='user-profile'),
+    path('trips/', TripView.as_view(), name='trip-list'),
+    path('trips/<int:pk>/', TripView.as_view(), name='trip-detail'),
 ]
